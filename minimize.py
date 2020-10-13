ec1bffde40c177fa5114fafe846997e383d86c9c
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2020-10-13 10:27:32 -0300

    Allow constraints in minimize.py

--- minimize.py
+++ minimize.py
@@ -6,8 +6,9 @@ import argparse
 
 from ase import Atoms
 from ase.optimize import BFGS
+from ase.constraints import FixAtoms
 from xtb.ase.calculator import XTB
-
+from mendeleev import element
 
 from rmsd import read_xyz, write_xyz
 from swing import smd2gbsa
@@ -22,6 +23,7 @@ def minimize(
     max_iterations=250,
     solvent="water",
     cache_api=True,
+    constraints=None,
 ):
     atoms = Atoms(numbers=atomno, positions=coord)
     calc = XTB(
@@ -33,6 +35,11 @@ def minimize(
         cache_api=cache_api,
     )
     atoms.set_calculator(calc)
+
+    if constraints is not None:
+        for c in constraints:
+            atoms.set_constraint(c)
+
     opt = BFGS(atoms)
     opt.run(fmax=0.05)
 
@@ -80,15 +87,50 @@ def main():
         help="Do not reuse generate API objects (not recommended)",
         action="store_false",
     )
+
+    parser.add_argument(
+        "--free-atoms",
+        help=(
+            "Only optimize the given atoms, as comma-separated one-based "
+            "indices, ranges or atomic symbols"
+        ),
+    )
     args = parser.parse_args()
     print(args)
 
     method = f"GFN{args.gfn}-xTB"
     solvent = smd2gbsa[args.solvent.lower()]
 
+    if args.free_atoms:
+        free_atom_indices = set()
+        free_atom_nos = set()
+        for i in args.free_atoms.split(","):
+            try:
+                free_atom_indices.add(int(i) - 1)
+            except ValueError:
+                if "-" in i:
+                    start_plus_one, end = (int(j) for j in i.split("-", 1))
+                    free_atom_indices.update(range(start_plus_one - 1, end))
+                else:
+                    free_atom_nos.add(
+                        element(i).atomic_number
+                    )  # atomic numbers
+
     for xyz_file in args.xyz_files:
         atomno, comment, coord = read_xyz(xyz_file)
         atomno, comment, coord = atomno[-1], comment[-1], coord[-1]
+
+        constraints = []
+        if args.free_atoms:
+            print(free_atom_indices)
+            print(free_atom_nos)
+            indices = list(
+                set(range(len(atomno)))
+                - free_atom_indices
+                - set(i for i, no in enumerate(atomno) if no in free_atom_nos)
+            )
+            print(f"Constraining atoms {indices}")
+            constraints.append(FixAtoms(indices=indices))
 
         atomno, coord = minimize(
             atomno,
@@ -99,6 +141,7 @@ def main():
             max_iterations=args.iterations,
             solvent=solvent,
             cache_api=args.cache_api,
+            constraints=constraints,
         )
 
         with open(xyz_file.name, "w") as stream:
